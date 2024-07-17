f767b8eb708652efa0540e708b752c682a63a3cc
CristiGitHubber <github@cgcristi.xyz>
2024-07-17 09:33:23 +0300

    add untracked files and directories

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SpotifyClient from './SpotifyClient';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SpotifyClient />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
